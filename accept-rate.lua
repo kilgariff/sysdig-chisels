31f38355ec84319d778f5d40323d8892d26abe00
root <root@ec2-54-90-5-17.compute-1.amazonaws.com>
2015-06-19 12:22:50 +0000

    Added accept rate chisel

---
+++ accept-rate.lua
@@ -0,0 +1,39 @@
+-- Chisel description
+description = "counts how many times the accept() system call has been invoked each second"
+short_description = "accept() rate"
+category = "misc"
+
+-- Chisel argument list
+args = {}
+
+-- Event counter
+count = 0
+
+-- Initialization callback
+function on_init()
+
+    -- Request the fields that we need
+    ftype = chisel.request_field("evt.type")
+    fdir = chisel.request_field("evt.dir")  
+   
+    -- respond to only the 'accept' syscall
+    chisel.set_filter("evt.type = accept and evt.dir = >")
+
+    -- set interval to 1 second
+    chisel.set_interval_s(1)
+
+    return true
+end
+
+-- Event parsing callback
+function on_event()
+    count = count + 1
+    return true
+end
+
+-- Interval callback
+function on_interval(ts_s, ts_ns, delta)
+    print(count)
+    count = 0
+    return true
+end
